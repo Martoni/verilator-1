798c3b489033e9208db708b3e7c11340fb73e399
Sebastien Van Cauwenberghe <svancau@gmail.com>
2018-06-24 18:28:58 +0200

    Add intial parser

--- src/V4VhdlParser.cpp
+++ src/V4VhdlParser.cpp
@@ -1,10 +1,20 @@
 #include "V4VhdlParser.h"
-#include <iostream>
-
-V4VhdlParser::V4VhdlParser() {
-  cout << "Creating parser" << endl;
-}
 
 void V4VhdlParser::parseFile(const string& filename) {
   cout << "Parsing " << filename << endl;
+
+  ifstream istr;
+  istr.open(filename);
+  if (!istr) {
+    cout << "Failed to open " << filename << endl;
+  }
+
+  ANTLRInputStream input_stream(istr);
+  vhdlLexer lexer(&input_stream);
+  CommonTokenStream tokens(&lexer);
+
+  vhdlParser parser(&tokens);
+  tree::ParseTree* tree = parser.design_file();
+
+  cout << tree->toStringTree(&parser) << endl << endl;
 }
--- src/V4VhdlParser.h
+++ src/V4VhdlParser.h
@@ -1,12 +1,17 @@
 #ifndef _V4VHDLPARSER_H_
 #define _V4VHDLPARSER_H_
 
+#include "antlr4-runtime.h"
+#include "vhdlLexer.h"
+#include "vhdlParser.h"
+#include <iostream>
 #include <string>
+
 using namespace std;
+using namespace antlr4;
 
 class V4VhdlParser {
 public:
-  V4VhdlParser();
   void parseFile(const string& filename);
 };
 
