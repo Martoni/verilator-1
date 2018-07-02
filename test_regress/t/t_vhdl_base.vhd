5d4512427b58be017c57eeeed8b605439fd18328
Sebastien Van Cauwenberghe <svancau@gmail.com>
2018-07-02 23:29:00 +0200

    Add other logic operators in test

--- test_regress/t/t_vhdl_base.vhd
+++ test_regress/t/t_vhdl_base.vhd
@@ -7,11 +7,20 @@ entity nand_nor_top is
            X1 : out  STD_LOGIC;     -- NAND gate output
            B1 : in  STD_LOGIC;      -- NOR gate input 1
            B2 : in  STD_LOGIC;      -- NOR gate input 2
-           Y1 : out  STD_LOGIC);    -- NOR gate output
+           Y1 : out  STD_LOGIC;    -- NOR gate output
+           Z1 : out  STD_LOGIC;    -- AND gate output
+           AA1 : out  STD_LOGIC;    -- OR gate output
+           AB1 : out  STD_LOGIC;    -- XOR gate output
+           AC1 : out  STD_LOGIC);    -- XNOR gate output
 end nand_nor_top;
 
 architecture Behavioral of nand_nor_top is
 begin
 X1 <= A1 nand A2;    -- 2 input NAND gate
 Y1 <= B1 nor B2;     -- 2 input NOR gate
+Z1 <= B1 and B2;     -- 2 input AND gate
+AA1 <= B1 or B2;     -- 2 input OR gate
+AB1 <= B1 xor B2;     -- 2 input XOR gate
+AC1 <= B1 xnor B2;     -- 2 input XNOR gate
+
 end Behavioral;
